136d66594727c12dc532ca395cf21bff4bd2c96b
Fredy Flores Lemus <fredy@Fredys-MacBook-Pro.local>
2026-05-13 20:17:46 -0600

    fix lambda function reference

--- terraform/api-gw.tf
+++ terraform/api-gw.tf
@@ -6,18 +6,18 @@ resource "aws_apigatewayv2_api" "main" {
 resource "aws_apigatewayv2_integration" "aws_lambda" {
   api_id                 = aws_apigatewayv2_api.main.id
   integration_type       = "AWS_PROXY"
-  integration_uri        = aws_lambda_function.main.invoke_arn
+  integration_uri        = aws_lambda_function.equipment_sme_assitant.invoke_arn
   payload_format_version = "2.0"
 }
 
 resource "aws_apigatewayv2_route" "post" {
-  api_id    = aws_apigateway2_api.main.id
+  api_id    = aws_apigatewayv2_api.main.id
   route_key = "POST /sme-assistant"
-  target    = "integrations/${aws_apigateway2_integration.aws_lambda.id}"
+  target    = "integrations/${aws_apigatewayv2_integration.aws_lambda.id}"
 }
 
 resource "aws_apigatewayv2_stage" "default" {
-  api_id      = aws_apigateway2_api.main.id
+  api_id      = aws_apigatewayv2_api.main.id
   name        = "$default"
   auto_deploy = true
 }
