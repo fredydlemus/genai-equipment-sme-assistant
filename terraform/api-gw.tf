546d734cb632315904c49b41587e9deebea41d7d
Fredy Flores Lemus <fredy@Fredys-MacBook-Pro.local>
2026-05-13 20:16:15 -0600

    fix terraform resources name

--- terraform/api-gw.tf
+++ terraform/api-gw.tf
@@ -1,22 +1,22 @@
-resource "aws_apigateway2_api" "main" {
+resource "aws_apigatewayv2_api" "main" {
   name          = "equipment-sme"
   protocol_type = "HTTP"
 }
 
-resource "aws_apigateway2_integration" "aws_lambda" {
-  api_id                 = aws_apigateway2_api.main.id
+resource "aws_apigatewayv2_integration" "aws_lambda" {
+  api_id                 = aws_apigatewayv2_api.main.id
   integration_type       = "AWS_PROXY"
   integration_uri        = aws_lambda_function.main.invoke_arn
   payload_format_version = "2.0"
 }
 
-resource "aws_apigateway2_route" "post" {
+resource "aws_apigatewayv2_route" "post" {
   api_id    = aws_apigateway2_api.main.id
   route_key = "POST /sme-assistant"
   target    = "integrations/${aws_apigateway2_integration.aws_lambda.id}"
 }
 
-resource "aws_apigateway2_stage" "default" {
+resource "aws_apigatewayv2_stage" "default" {
   api_id      = aws_apigateway2_api.main.id
   name        = "$default"
   auto_deploy = true
